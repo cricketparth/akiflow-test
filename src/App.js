c493f69ea0dfcd627c4a26543229821de2ee1a7e
cricketparth <50546286+cricketparth@users.noreply.github.com>
2020-10-03 11:15:03 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+AIzaSyC5uN1aYJmZoImObGu8GBDM5qLRIMObbII//api key
+435241275040-dsh2lrqmbv0p6j92bcnjv2s4dk3m20hg.apps.googleusercontent.com //client id
+sSRyjNOBt170KdtR5JojyWHx // client secret
